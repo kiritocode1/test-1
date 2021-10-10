b328c8db4ca7d185f92208912f7797b8390ee53e
kirito code1 <kathawalearyan9@gmail.com>
2021-10-10 15:28:16 +0530

    no

--- 3list_problem.py
+++ 3list_problem.py
@@ -16,25 +16,26 @@ answer()
 
 #! ///////////////////////////////////////////////////////////////////////////////////////////////////////
 #  TODO: u have 3 lists find the min index with sum given 
-def finalFantasy(x:list, y: list , z: list , a:int)->Any:
-    ''' this code is essentially the same as n diff
-    i made but hey it's awesome :) 
+# def finalFantasy(x:list, y: list , z: list , a:int)->Any:
+#     ''' this code is essentially the same as n diff
+#     i made but hey it's awesome :) 
     
-    x --->[1,2,3]
-    y --->[4,5,6]
-    z --->[7,8,9] 
-    this code does take care because iteration will return the minimum
-    index. 
-    '''
-    for i in range(len(x)):
-        for j in range(len(x)):
-            diff = a-(x[i]+y[j])
-            try: return [i , j , z.index(diff)]
-            except: continue
-    return None
-
-
-
+#     x --->[1,2,3]
+#     y --->[4,5,6]
+#     z --->[7,8,9] 
+#     this code does take care because iteration will return the minimum
+#     index. 
+#     '''
+#     for i in range(len(x)):
+#         for j in range(len(x)):
+#             diff = a-(x[i]+y[j])
+#             try: return [i , j , z.index(diff)]
+#             except: continue
+#     return None
+#  ! ////////////////////////////////////////////////////////////////////////
+import pyqrcode
+a =pyqrcode.create(input("here: "))
+print(a.png("test-1.png",scale=5))
 
 
 
