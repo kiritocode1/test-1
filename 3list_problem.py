f196e33074a59b47c976ebb7ed255a86186118da
kirito code1 <kathawalearyan9@gmail.com>
2021-10-10 15:07:35 +0530

    done

--- 3list_problem.py
+++ 3list_problem.py
@@ -1,7 +1,7 @@
 
-
 # TODO :  make a namedtuple or a dataframe , easily . 	
 from collections import namedtuple
+from typing import Any
 def answer():
     no_of_students=int(input("here ---> "))
     columns_of_adjectives = input("the columns of data here ---> ").split()
@@ -14,14 +14,25 @@ def answer():
     return print(list_of_students)
 answer()
 
-# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+#! ///////////////////////////////////////////////////////////////////////////////////////////////////////
 #  TODO: u have 3 lists find the min index with sum given 
-def finalFantasy(x:list, y: list , z: list , a: int):
+def finalFantasy(x:list, y: list , z: list , a:int)->Any:
+    ''' this code is essentially the same as n diff
+    i made but hey it's awesome :) 
+    
+    x --->[1,2,3]
+    y --->[4,5,6]
+    z --->[7,8,9] 
+    this code does take care because iteration will return the minimum
+    index. 
+    '''
     for i in range(len(x)):
         for j in range(len(x)):
             diff = a-(x[i]+y[j])
             try: return [i , j , z.index(diff)]
             except: continue
+    return None
+
 
 
 
