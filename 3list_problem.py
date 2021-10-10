97d988753d8f6e215e8ea861844a4dda2451ecdd
kirito <78982932+kiritocode1@users.noreply.github.com>
2021-10-10 14:49:57 +0530

    Update 3list_problem.py

--- 3list_problem.py
+++ 3list_problem.py
@@ -13,3 +13,17 @@ def answer():
         list_of_students.append(xyz)
     return print(list_of_students)
 answer()
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+#  TODO: u have 3 lists find the min index with sum given 
+def finalFantasy(x:list, y: list , z: list , a: int):
+    for i in range(len(x)):
+        for j in range(len(x)):
+            diff = a-(x[i]+y[j])
+            try: return [i , j , z.index(diff)]
+            except: continue
+
+
+
+
+
