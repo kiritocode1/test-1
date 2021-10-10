5388d1114b702b0eabcb6d1033213a167243b068
kirito code1 <kathawalearyan9@gmail.com>
2021-10-10 13:27:59 +0530

    nice

--- 3list_problem.py
+++ 3list_problem.py
@@ -26,8 +26,11 @@ from collections import namedtuple
 def answer():
     no_of_students=int(input("here ---> "))
     columns_of_adjectives = input("the columns of data here ---> ").split()
-    a = namedtuple("students" , columns_of_adjectives)
+    a = namedtuple("fofo" , columns_of_adjectives)
     list_of_students = []
     for i in range(no_of_students):
         data = input("here----> ").split()
-        
+        xyz = a(*data)
+        list_of_students.append(xyz)
+    return print(list_of_students)
+answer()
