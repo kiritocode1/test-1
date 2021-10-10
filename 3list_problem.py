a07563c78650b40f6f00c1c5f6cb43805189dc79
kirito code1 <kathawalearyan9@gmail.com>
2021-10-10 13:07:59 +0530

    nice

---
+++ 3list_problem.py
@@ -0,0 +1,33 @@
+
+# TODO: to make  some shit
+
+# def foo(n):
+#     nice= nt("fofo" ,input("here--->"))
+#     list1 = []
+#     for i in range(n):
+
+# Python code to demonstrate namedtuple()
+	
+from collections import namedtuple
+	
+# # Declaring namedtuple()
+# Student = namedtuple('Student',['name','age','DOB'])
+	
+# # Adding values
+# S = Student('Nandini','19','2541997')
+	
+# # Access using index
+# print ("The Student age using index is : ",end ="")
+# print (S[1])
+	
+# # Access using name
+# print ("The Student name using keyname is : ",end ="")
+# print (S.name)
+def answer():
+    no_of_students=int(input("here ---> "))
+    columns_of_adjectives = input("the columns of data here ---> ").split()
+    a = namedtuple("students" , columns_of_adjectives)
+    list_of_students = []
+    for i in range(no_of_students):
+        data = input("here----> ").split()
+        
